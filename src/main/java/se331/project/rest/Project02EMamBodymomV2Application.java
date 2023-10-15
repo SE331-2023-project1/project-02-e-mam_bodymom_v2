352afa66893d7ef2e57e887960f6293a7c7e2b34
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-10-15 19:02:56 +0700

    initailize

---
+++ src/main/java/se331/project/rest/Project02EMamBodymomV2Application.java
@@ -0,0 +1,13 @@
+package se331.project.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02EMamBodymomV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project02EMamBodymomV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/se331/project/rest/Project02EMamBodymomV2BetaApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.project.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project02EMamBodymomV2BetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
