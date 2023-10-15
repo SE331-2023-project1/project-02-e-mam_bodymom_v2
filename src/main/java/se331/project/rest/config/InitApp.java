3539ebacb38e24b981ad5db00cc7951c66feae03
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-10-15 22:41:54 +0700

    Add InitApp TeacherDao and TeacherController

--- src/main/java/se331/project/rest/config/InitApp.java
+++ src/main/java/se331/project/rest/config/InitApp.java
@@ -5,12 +5,16 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.project.rest.entity.Student;
+import se331.project.rest.entity.Teacher;
 import se331.project.rest.repository.StudentRepository;
+import se331.project.rest.repository.TeacherRepository;
+
 
 @Component
 @RequiredArgsConstructor
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     final StudentRepository studentRepository;
+    final TeacherRepository teacherRepository;
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
         studentRepository.save(Student.builder()
@@ -22,5 +26,16 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         studentRepository.save(Student.builder()
                 .name("Pattanachai")
                 .surname("Sakunchao").build());
+
+
+        teacherRepository.save(Teacher.builder()
+                .name("Dto")
+                .surname("Dto").build());
+        teacherRepository.save(Teacher.builder()
+                .name("Kong")
+                .surname("Passakorn").build());
+        teacherRepository.save(Teacher.builder()
+                .name("Tei")
+                .surname("Pathathai").build());
     }
 }
