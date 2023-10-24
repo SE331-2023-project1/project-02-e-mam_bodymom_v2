a5b5e26d903927d2798be40d2da4c04761ee5cac
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2023-10-24 10:30:18 +0700

    Check Host server

--- src/main/java/se331/project/rest/Project02EMamBodymomV2Application.java
+++ src/main/java/se331/project/rest/Project02EMamBodymomV2Application.java
@@ -23,6 +23,7 @@ public class Project02EMamBodymomV2Application {
 						.allowedOrigins("http://18.208.106.67:8001").exposedHeaders("x-total-count")
 						.allowedMethods("GET","POST","PUT","DELETE","HEAD","OPITOINS")
 						.allowCredentials(true)
+						.allowedOrigins("*")
 						.allowedHeaders("Origin","Authorization","application/json","Content-Type");
 
 			}
